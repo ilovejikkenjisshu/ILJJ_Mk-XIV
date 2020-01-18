c1ea7c71624ce7dfc7a4f46ffff9c687635f66fa
nuller-jo <nuller.jmain@gmail.com>
2020-01-18 17:55:16 +0900

    ボットが動くようにファイルを追加

---
+++ main.js
@@ -0,0 +1,43 @@
+//モジュールの読み込み
+var keys = require('./modules/keys.js');
+
+// ---------- discord ----------
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//botのログイン処理
+client.login(keys.discord);
+
+//botの準備ができたら呼ばれる
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(client);
+});
+
+client.on('message', msg => {
+    //コマンド系の場合
+    if(msg.content.startsWith('/xiv')){
+	commandCont(msg);
+    }
+    //コマンド系の場合
+    if(msg.content.startsWith('/xiv ping')){
+        msg.channel.send('hello!!');
+    }
+
+});
+
+//コマンドをさばくメソッド
+function commandCont(msg){
+    if (msg.content.startsWith('%tts')){
+	//ttsコマンドをスマホからでもできるように代行する
+	tts(msg);
+    }
+}
+
+function tts(msg){
+    //スペースで分割
+    var content = msg.content.slice(4);
+    var op={tts:true};
+    var cu = client.user;
+    msg.channel.send(msg.author.username+ 'のメッセージを代行します、' + content,op).then(message => message.delete());
+}
